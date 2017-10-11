c8b31b3fde90c0b3bac8a1794c5df4e21d201648
Florian Büchner <flori.buechner90@googlemail.com>
2017-10-11 12:30:28 +0200

    add new files with namespaces

---
+++ Configuration/Configuration.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Configuration;
+/**
+ * Class Configuration
+ */
+class Configuration
+{
+    /**
+     * @var array
+     */
+    private static $registry = Array();
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public static function set($key,$value)
+    {
+        self::$registry[$key] = $value;
+    }
+
+    /**
+     * @param $key
+     * @return bool|mixed
+     */
+    public static function get($key)
+    {
+        if(array_key_exists($key,self::$registry)){
+            return self::$registry[$key];
+        }
+        return false;
+    }
+}
